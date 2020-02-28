1706cfe94efc01d401eadcbe74342c3c0c98bfe3
Simon Schubert <si.schubert@live.de>
2020-02-28 22:12:11 +0100

    Import Reg Test

--- fixtures/candy.sql
+++ fixtures/candy.sql
@@ -2,16 +2,16 @@ DELETE FROM "review";
 DELETE FROM "candy_translation";
 DELETE FROM "candy";
 
-INSERT INTO "candy" ("id","gtin","weight")
-VALUES (1,'886037363214',5),(2,'9272037363324',10),(3,'5567037363214',15),(4,'893037363214',20);
+INSERT INTO "candy" ("gtin","weight")
+VALUES ('886037363214',5),('9272037363324',10),('5567037363214',15),('893037363214',20);
 
-INSERT INTO "candy_translation" ("id","candy_id","language","title")
+INSERT INTO "candy_translation" ("candy_id","language","title")
 VALUES 
-(DEFAULT, 1,'en','White Choclate Crisp'),
-(DEFAULT, 1,'de','Weiße Schokolade mit Krisp'),
-(DEFAULT, 2,'en','Peanut Butter Cup'),
-(DEFAULT, 2,'de','Erdnuss Cups'),
-(DEFAULT, 3,'en','Dark Chocolate'),
-(DEFAULT, 3,'de','Zartbitter Schokolade'),
-(DEFAULT, 4,'en','Princess Cake'),
-(DEFAULT, 4,'de','Prinzessinen Rolle');
+((SELECT id FROM candy WHERE gtin = '886037363214'),'en','White Choclate Crisp'),
+((SELECT id FROM candy WHERE gtin = '886037363214'),'de','Weiße Schokolade mit Krisp'),
+((SELECT id FROM candy WHERE gtin = '9272037363324'),'en','Peanut Butter Cup'),
+((SELECT id FROM candy WHERE gtin = '9272037363324'),'de','Erdnuss Cups'),
+((SELECT id FROM candy WHERE gtin = '5567037363214'),'en','Dark Chocolate'),
+((SELECT id FROM candy WHERE gtin = '5567037363214'),'de','Zartbitter Schokolade'),
+((SELECT id FROM candy WHERE gtin = '893037363214'),'en','Princess Cake'),
+((SELECT id FROM candy WHERE gtin = '893037363214'),'de','Prinzessinen Rolle');
---
+++ tests/Controller/ImportControllerTest.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ImportControllerTest extends WebTestCase
+{
+    private Connection $connection;
+
+    private $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+
+        /** @var EntityManagerInterface $em */
+        $em = self::$container->get('doctrine.orm.default_entity_manager');
+        $this->connection = $em->getConnection();
+        $this->connection->exec(file_get_contents(__DIR__ . '/../../fixtures/candy.sql'));
+    }
+
+    public function testImport(): void
+    {
+        $this->client->request(
+            'POST',
+            '/import',
+            [],
+            [],
+            self::HEADERS,
+            $this->importPayload
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $created = $this->connection->fetchAll($this->importSql);
+
+        $this->assertEquals(20, count($created));
+    }
+
+    private const HEADERS = [
+        'HTTP_ACCEPT' => 'application/json',
+        'HTTP_ACCEPT_LANGUAGE' => 'de_DE',
+        'CONTENT_TYPE' => 'application/json'
+    ];
+
+    private $importSql = <<<'EOT'
+    SELECT * FROM candy 
+        JOIN candy_translation ON (candy.id = candy_translation.candy_id)
+        WHERE gtin IN (
+            '9372610283610', 
+            '7682610283411', 
+            '9372610283612',
+            '7682610283413', 
+            '9372610283614', 
+            '7682610283415', 
+            '9372610283616', 
+            '7682610283417', 
+            '9372610283618', 
+            '7682610283419',
+            '9372610283620'
+        )
+    EOT;
+
+    private $importPayload = <<<'EOT'
+    [
+        {
+            "gtin": "9372610283610",
+            "weight": 10,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "7682610283411",
+            "weight": 11,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "9372610283612",
+            "weight": 12,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "7682610283413",
+            "weight": 13,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "9372610283614",
+            "weight": 14,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "7682610283415",
+            "weight": 15,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "9372610283616",
+            "weight": 16,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "7682610283417",
+            "weight": 17,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "9372610283618",
+            "weight": 18,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        },
+        {
+            "gtin": "7682610283419",
+            "weight": 19,
+            "translations": [
+                {
+                    "language": "de",
+                    "title": "Weiße Schokolade"
+                },
+                {
+                    "language": "en",
+                    "title": "White Chocolate"
+                }
+            ]
+        }
+    ]
+    EOT;
+}
