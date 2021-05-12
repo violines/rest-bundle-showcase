22a5e1e331a1100650dfa5ae956f50107ab0019d
Simon Schubert <48808627+simon-schubert@users.noreply.github.com>
2021-05-12 22:14:43 +0200

    paginate query (#38)

--- src/Domain/Product/Query/FilterProducts.php
+++ src/Domain/Product/Query/FilterProducts.php
@@ -15,6 +15,8 @@ final class FilterProducts
 
     public string $language = 'en';
 
+    public int $page = 1;
+
     public function setRatingFrom(string $ratringFrom)
     {
         $this->ratingFrom = (int) $ratringFrom;
@@ -23,5 +25,15 @@ final class FilterProducts
     public function setRatingTo(string $ratringTo)
     {
         $this->ratingTo = (int) $ratringTo;
+    }
+
+    public function setPage(string $page)
+    {
+        if (1 < (int)$page) {
+            $this->page = (int)$page;
+            return;
+        }
+
+        $this->page = 1;
     }
 }
